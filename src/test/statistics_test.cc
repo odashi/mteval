8d35dd98ce0c5fe5fa929cf5fb013715dba39f47
odashi <yus.takara@gmail.com>
2015-12-24 01:47:29 +0900

    add test suite

---
+++ src/test/statistics_test.cc
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(StatisticsTest)
+
+BOOST_AUTO_TEST_CASE(test) {
+  BOOST_CHECK_EQUAL(0, 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
