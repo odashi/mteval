726c1d823718a80e5190562a9e7f64ec97217d03
Yusuke Oda <yus.takara@gmail.com>
2017-11-28 13:03:56 +0900

    Fix static linking problem in test cases.

--- test/statistics_test.cc
+++ test/statistics_test.cc
@@ -1,5 +1,6 @@
-#define BOOST_TEST_MAIN
-#include <boost/test/included/unit_test.hpp>
+#define BOOST_TEST_MODULE statistics_test
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
 
 #include <mteval/Statistics.h>
 
